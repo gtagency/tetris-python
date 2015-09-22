2811b96b94590ec588a04eafdc05140079746e47
Kenneth Adams <khetnen@gmail.com>
2015-09-22 19:36:17 -0400

    basic heuristics

--- Bot/Strategies/BasicStrategy.py
+++ Bot/Strategies/BasicStrategy.py
@@ -1,6 +1,28 @@
-class BasicStrategy:
+from random import randint
+
+from AbstractStrategy import AbstractStrategy
+
+
+class BasicStrategy(AbstractStrategy):
     def __init__(self, game):
-        self._game = game
+        AbstractStrategy.__init__(self, game)
+        self._actions = ['left', 'right', 'turnleft', 'turnright', 'down', 'drop']
+
+    def score_heuristic(self, new_field):
+        for row in new_field.field:
+            if all(row):
+                return True
+        return False
+
+    def height_heuristic(self, new_field):
+        for row in new_field.rows:
+            for i, row in enumerate(new_field.field):
+                if any(row):
+                    return new_field.height - i
 
     def choose(self):
-        raise NotImplementedError("Please Implement this method")
+        ind = [randint(0, 4) for _ in range(1, 10)]
+        moves = map(lambda x: self._actions[x], ind)
+        moves.append('drop')
+
+        return moves
