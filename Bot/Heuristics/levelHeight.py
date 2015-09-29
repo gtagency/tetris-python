614c90e9eda708a9fbdc0df9807f91563d0868fd
Joshua Morton <joshua.morton13@gmail.com>
2015-09-29 18:28:41 -0400

    refactored levelHeight
    the function is now much more pythonic

--- Bot/Heuristics/levelHeight.py
+++ Bot/Heuristics/levelHeight.py
@@ -1,12 +1,7 @@
 def levelHeight(field):
     """Given a field, return the height of the highest block, normalized."""
     # this is probably wrong. There is no documentation in this thing. Ugh.
-    height = 0
-    for i in reversed(range(len(field))):
-        for j in range(len(field[0])):
-            if field[i][j] == 1:
-                height = i
-                break
-    normalizedHeight = float(height) / len(field)
-    return normalizedHeight
+    for i in reversed(field):
+        if not all(i):
+            return i-1 / len(field)
 
