ecf253321241727518b2896fda27f1b3db72afc4
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2015-12-01 18:03:16 -0500

    some mc stuff ahh

--- Bot/Strategies/BasicStrategy.py
+++ Bot/Strategies/BasicStrategy.py
@@ -1,27 +1,25 @@
 from random import randint
-from Heuristics.levelHeight import levelHeight
-from Heuristics.scoreCalc import scoreCalc
 from AbstractStrategy import AbstractStrategy
 from math import log
 
 def score_heuristic(new_field):
-    for row in new_field.field:
+    score = 0
+    for row in field:
         if all(row):
-            return True
-    return False
+            score += 1
+    return float(score) / len(new_field.field)
 
 def height_heuristic(new_field):
-    for row in new_field.rows:
-        for i, row in enumerate(new_field.field):
-            if any(row):
-                return new_field.height - i
+    for row in new_field:
+        if any(row):
+            return float(len(new_field) - i) / len(new_field)
 
 def dfs(node, depth):
     currentDepth = depth
     for child in node.getAllChildren():
         if not child in node.children and currentDepth > 0:
             node.children.append(child)
-            dfs(child, currentDepth--)
+            dfs(child, currentDepth-1)
 
 class BasicStrategy(AbstractStrategy):
     def __init__(self, game):
@@ -66,12 +64,13 @@ class MonteCarloStrategy(AbstractStrategy):
         # exploration / exploitation
         score = lambda x: ((float(x.reward) / x.visits) + C * (2 * log(float(x.visits) / currentVisits))**(1/2))
         childList = root.children
-        best = max([(score(child), child) for child in childList])
+        _, best = max([(score(child), child) for child in childList])
+        return best
 
     def evaluate(self, root):
         weights = [1, 1]
         field = root.state.field
-        score = weights[0] * scoreCalc(field) + weights[1] * levelHeight(field)
+        score = weights[0] * score_heuristic(field) + weights[1] * height_heuristic(field)
         return float(score) / sum(weights)
 
     def searchMCBranch(self, root):
@@ -100,15 +99,18 @@ class MonteCarloStrategy(AbstractStrategy):
         tree = self.generateMCTree()
 
         # Pick a goal.
-        goal = self.searchMCTree(tree)
+        goal = self.searchMCTree(tree).state
 
         # Find actions to goal.
         # TODO
 
+        # let's just output the goal we want:
+        print goal
+
         # Fallback to random strategy.
-        # ind = [randint(0, 4) for _ in range(1, 10)]
-        # moves = map(lambda x: self._actions[x], ind)
-        # moves.append('drop')
+        ind = [randint(0, 4) for _ in range(1, 10)]
+        moves = map(lambda x: self._actions[x], ind)
+        moves.append('drop')
 
         return moves
 
