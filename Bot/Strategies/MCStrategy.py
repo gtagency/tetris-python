0cdd6f43d452002c042420a7bf2b103f67071318
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2015-12-22 16:28:35 -0200

    next selection now works with new MC and uses phantom nodes to handle unvisited nodes

--- Bot/Strategies/MCStrategy.py
+++ Bot/Strategies/MCStrategy.py
@@ -24,7 +24,7 @@ class Node(object):
         for piece in pieces:
             self.possibleChildren.extend([(rotation, position)
                                           for rotation in piece._rotations
-                                          for position in positions])
+                                          for position in range(len(self.state[0])]))
 
     def getNextChild(self):
         """Returns a child Node with a randomly picked piece, rotation and position."""
@@ -40,6 +40,7 @@ class Node(object):
         """Returns a child Node with the rotation dropped from pos."""
         new_state = self.state.projectRotationDown(rotation, pos)
         child = Node(new_state, self, rot_and_pos=(rotation, pos), this_piece=self.next_piece)
+        self.children.append(child)
         return child
 
 
@@ -47,9 +48,10 @@ class MonteCarloStrategy(AbstractStrategy):
     def __init__(self, game):
         AbstractStrategy.__init__(self, game)
         self._actions = ['left', 'right', 'turnleft', 'turnright', 'down', 'drop']
-        self.score = lambda x: (
+        self.C = 2 ** (0.5)
+        self.score = lambda x, totalVisits: (
                 (float(x.reward) / x.visits) +
-                C * (log(currentVisits) / float(x.visits))**(0.5))
+                self.C * (log(totalVisits) / float(x.visits))**(0.5))
 
     def has_full_line(self, field):
         for row in field:
@@ -72,21 +74,13 @@ class MonteCarloStrategy(AbstractStrategy):
 
     def pickBestChild(self, root, final=False, isTreeRoot=False):
         C = 2**(0.5)
-        currentVisits = root.visits
-        childList = root.children
+        totalVisits = root.visits
 
-        if currentVisits == 0:
-            return choice(childList)
-        else:
-            # exploration / exploitation
-            if isTreeRoot and False:
-                sys.stderr.write(str([(score(child), child.visits) for child in childList]) + '\n')
-            sc, best = max([(score(child), child) for child in childList])
-            if final and False:
-                sys.stderr.write(str(best.state.field) + '\n')
-                sys.stderr.write(str(sc) + '\n')
-                sys.stderr.write(str(self.evaluate(best)) + '\n')
-            return best
+        _, best = max([(self.score(child, totalVisits), child) for child in root.children] +
+                      [(self.score({ visits: 1, reward: 0 }, totalVisits), 'phantom')])
+        if best === 'phantom':
+            best = root.getNextChild()
+        return best
 
     def evaluate(self, root, debug=False):
         """Returns utility of a state.
@@ -127,14 +121,10 @@ class MonteCarloStrategy(AbstractStrategy):
 
     def choose(self):
         # Generate Monte Carlo Tree.
-        tree, avgUtil = self.generateMCTree()
+        tree = self.generateMCTree()
 
         # Pick a goal.
-        # goal = self.searchMCTree(tree, avgUtil).state
-        goal = self.searchMCTree(tree, avgUtil)
-        # TODO: this avgutil is not working because it is setting the AVERAGE case to be desirable.
-        # this means that MC will explore every branch uniformly because the average case is spread uniformly throughout the tree.
-        # This might be fixed if we add new heuristics like holes are bad, lines are good, etc.
+        goal = self.searchMCTree(tree, avgUtil).state
 
         ### --- ###
         # currentVisits = tree.visits
@@ -153,74 +143,73 @@ class MonteCarloStrategy(AbstractStrategy):
             # sys.stderr.write('chance:' + str(score(child)) + '\n')
             # i += 1
 
-        sys.stderr.write('avgUtil: ' + str(avgUtil) + '\n')
-        sys.stderr.write('root visits: ' + str(tree.visits) + '\n')
-        sys.stderr.write('root reward: ' + str(tree.reward) + '\n')
+        # sys.stderr.write('avgUtil: ' + str(avgUtil) + '\n')
+        # sys.stderr.write('root visits: ' + str(tree.visits) + '\n')
+        # sys.stderr.write('root reward: ' + str(tree.reward) + '\n')
 
-        childVisits = [(child.visits, child.reward) for child in tree.children]
-        sys.stderr.write('childVisitsAndRewards: \n' + str(childVisits) + '\n')
+        # childVisits = [(child.visits, child.reward) for child in tree.children]
+        # sys.stderr.write('childVisitsAndRewards: \n' + str(childVisits) + '\n')
 
-        childVisits = [(child.visits, child.reward) for child in tree.children[0].children]
-        sys.stderr.write('childVisitsAndRewards2: \n' + str(childVisits) + '\n')
+        # childVisits = [(child.visits, child.reward) for child in tree.children[0].children]
+        # sys.stderr.write('childVisitsAndRewards2: \n' + str(childVisits) + '\n')
 
-        sys.stderr.write('goal: \n' + str(goal.state.field))
-        sys.stderr.write('goal util: ' + str(self.evaluate(goal)))
+        # sys.stderr.write('goal: \n' + str(goal.state.field))
+        # sys.stderr.write('goal util: ' + str(self.evaluate(goal)))
         ### --- ###
 
-        goal = goal.state
-
         # Find actions to goal.
-        actions = self.reverseDFS(goal, self._game.piecePosition, self._game.piece)
+        # actions = self.reverseDFS(goal, self._game.piecePosition, self._game.piece)
+        actions = []
         actions.append('drop')
 
         return actions
 
-    def reverseDFS(self, goal, piecePos, piece):
+    # def reverseDFS(self, goal, piecePos, piece):
 
-        closed = set()
-        currentField = self._game.me.field
-        openList = [(piecePos, piece, [])]
-        # print goal.field
-
-
-        while len(openList) != 0:
-            piecePos, piece, intstructions = openList.pop()
-
-            test = goal.field == currentField.fitPiece(piece.positions(), piecePos)
-            if type(test) is not bool and test.all():
-                #(type(test) is bool and test == True) or
-                # print currentField.fitPiece(piece.positions(), piecePos)
-                # print piecePos
-                # print piece.positions()
-                # print piece._rotateIndex
-                return intstructions
-
-            #rotations
-            copyPiece = copy.deepcopy(piece)
-            if copyPiece.turnLeft() and currentField._checkIfPieceFits(currentField._offsetPiece(copyPiece.positions(), piecePos)):
-                newState = (copy.deepcopy(piecePos), copy.deepcopy(copyPiece), copy.deepcopy(intstructions))
-                newState[2].append("turnleft")
-                if (tuple(newState[0]), newState[1]._rotateIndex) not in closed:
-                    openList.append(newState)
-                    closed.add((tuple(newState[0]), newState[1]._rotateIndex))
-            copyPiece = copy.deepcopy(piece)
-            if copyPiece.turnRight() and currentField._checkIfPieceFits(currentField._offsetPiece(copyPiece.positions(), piecePos)):
-                newState = (copy.deepcopy(piecePos), copy.deepcopy(copyPiece), copy.deepcopy(intstructions))
-                newState[2].append("turnright")
-                if (tuple(newState[0]), newState[1]._rotateIndex) not in closed:
-                    openList.append(newState)
-                    closed.add((tuple(newState[0]), newState[1]._rotateIndex))
+        # closed = set()
+        # currentField = self._game.me.field
+        # openList = [(piecePos, piece, [])]
+        # # print goal.field
 
 
-            #normal moves
-            for move in [[1,0],[-1,0],[0,1]]:
-                offset = currentField._offsetPiece(piece.positions(), map(add, piecePos, move))
-                if currentField._checkIfPieceFits(offset):
-                    newState = (map(add, piecePos, move), copy.deepcopy(piece), copy.deepcopy(intstructions))
-                    newState[2].append("left" if move == [-1,0] else ("right" if move == [1,0] else "down"))
-                    if (tuple(newState[0]), newState[1]._rotateIndex) not in closed:
-                        openList.append(newState)
-                        closed.add((tuple(newState[0]), newState[1]._rotateIndex))
+        # while len(openList) != 0:
+            # piecePos, piece, intstructions = openList.pop()
+
+            # test = goal.field == currentField.fitPiece(piece.positions(), piecePos)
+            # if type(test) is not bool and test.all():
+                # #(type(test) is bool and test == True) or
+                # # print currentField.fitPiece(piece.positions(), piecePos)
+                # # print piecePos
+                # # print piece.positions()
+                # # print piece._rotateIndex
+                # return intstructions
+
+            # #rotations
+            # copyPiece = copy.deepcopy(piece)
+            # if copyPiece.turnLeft() and currentField._checkIfPieceFits(currentField._offsetPiece(copyPiece.positions(), piecePos)):
+                # newState = (copy.deepcopy(piecePos), copy.deepcopy(copyPiece), copy.deepcopy(intstructions))
+                # newState[2].append("turnleft")
+                # if (tuple(newState[0]), newState[1]._rotateIndex) not in closed:
+                    # openList.append(newState)
+                    # closed.add((tuple(newState[0]), newState[1]._rotateIndex))
+            # copyPiece = copy.deepcopy(piece)
+            # if copyPiece.turnRight() and currentField._checkIfPieceFits(currentField._offsetPiece(copyPiece.positions(), piecePos)):
+                # newState = (copy.deepcopy(piecePos), copy.deepcopy(copyPiece), copy.deepcopy(intstructions))
+                # newState[2].append("turnright")
+                # if (tuple(newState[0]), newState[1]._rotateIndex) not in closed:
+                    # openList.append(newState)
+                    # closed.add((tuple(newState[0]), newState[1]._rotateIndex))
 
 
-        return None
+            # #normal moves
+            # for move in [[1,0],[-1,0],[0,1]]:
+                # offset = currentField._offsetPiece(piece.positions(), map(add, piecePos, move))
+                # if currentField._checkIfPieceFits(offset):
+                    # newState = (map(add, piecePos, move), copy.deepcopy(piece), copy.deepcopy(intstructions))
+                    # newState[2].append("left" if move == [-1,0] else ("right" if move == [1,0] else "down"))
+                    # if (tuple(newState[0]), newState[1]._rotateIndex) not in closed:
+                        # openList.append(newState)
+                        # closed.add((tuple(newState[0]), newState[1]._rotateIndex))
+
+
+        # return None
